87073012ad5bcbee613fc49f2a572886a6cf2dca
sougou1323 <546619116@qq.com>
2019-10-13 17:39:05 +0800

    websocket

---
+++ src/main/java/_11/WebSocketServer.java
@@ -0,0 +1,50 @@
+package _11;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @author huang
+ * @version v1.0
+ * @date 2019-10-13 4:12 PM
+ */
+public class WebSocketServer {
+
+    public void run(int port) throws InterruptedException {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast("http-codec", new HttpServerCodec())
+                                    .addLast("aggregator", new HttpObjectAggregator(65536))
+                                    .addLast("http-chunked", new ChunkedWriteHandler())
+                                    .addLast("handler", new WebSocketServerHandler());
+                        }
+                    });
+
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new WebSocketServer().run(8080);
+    }
+
+}
---
+++ src/main/java/_11/WebSocketServerHandler.java
@@ -0,0 +1,107 @@
+package _11;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.util.CharsetUtil;
+
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static io.netty.handler.codec.http.HttpHeaders.isKeepAlive;
+import static io.netty.handler.codec.http.HttpHeaders.setContentLength;
+
+/**
+ * @author huang
+ * @version v1.0
+ * @date 2019-10-13 4:18 PM
+ */
+public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> {
+
+    private static final Logger logger = Logger.getLogger(WebSocketServerHandler.class.getName());
+
+    private WebSocketServerHandshaker handshaker;
+
+    @Override
+    protected void messageReceived(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (msg instanceof FullHttpRequest) {
+            handleHttpRequest(ctx, (FullHttpRequest)msg);
+        }
+
+        if (msg instanceof WebSocketFrame) {
+            handleWebSocketFrame(ctx, (WebSocketFrame)msg);
+        }
+    }
+
+    private void handleWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) {
+        if (frame instanceof CloseWebSocketFrame) {
+            handshaker.close(ctx.channel(), (CloseWebSocketFrame)frame.retain());
+            return;
+        }
+
+        if (frame instanceof PingWebSocketFrame) {
+            ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
+            return;
+        }
+
+        if (!(frame instanceof TextWebSocketFrame)) {
+            throw new UnsupportedOperationException(String.format(
+                    "%s frame types not supported", frame.getClass().getName()));
+        }
+
+        String request = ((TextWebSocketFrame) frame).text();
+        if (logger.isLoggable(Level.FINE)) {
+            logger.fine(String.format("%s received %s", ctx.channel(), request));
+        }
+
+        ctx.channel().write(
+                new TextWebSocketFrame(request + "你好, 北京时间 ：" + new Date().toString()));
+    }
+
+    private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) {
+        if (!req.getDecoderResult().isSuccess() || !"websocket".equals(req.headers().get("Upgrade"))) {
+            sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST));
+            return ;
+        }
+
+        WebSocketServerHandshakerFactory wsFactory =
+                new WebSocketServerHandshakerFactory("ws://localhost:8080/websocket", null, false);
+        handshaker = wsFactory.newHandshaker(req);
+        if (handshaker == null) {
+            WebSocketServerHandshakerFactory.sendUnsupportedWebSocketVersionResponse(ctx.channel());
+        } else {
+            handshaker.handshake(ctx.channel(), req);
+        }
+    }
+
+    private void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest req, DefaultFullHttpResponse resp) {
+        if (resp.getStatus().code() != 200) {
+            ByteBuf buf = Unpooled.copiedBuffer(resp.getStatus().toString(), CharsetUtil.UTF_8);
+            resp.content().writeBytes(buf);
+            buf.release();
+            setContentLength(resp, resp.content().readableBytes());
+        }
+
+        ChannelFuture f = ctx.channel().writeAndFlush(resp);
+        if (!isKeepAlive(req) || resp.getStatus().code() != 200) {
+            f.addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
